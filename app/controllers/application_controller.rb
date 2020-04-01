ddb63543e455af524cf1d925ec000d7e9ab6e751
Lynie McClimon <vmcclimon33@yahoo.com>
2020-04-01 01:23:31 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def cart
-    # set cart_id
-    session[:cart_id] = @cart.id
-
-    # load the cart referenced in the session
-    @cart = Cart.find(session[:cart_id])
+    session[:cart] ||= []
   end
 
 end
